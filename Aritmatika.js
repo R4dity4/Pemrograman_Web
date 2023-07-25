57b73fd8e7c529cfd81279826bded4e085ed4739
R4dity4 <140366601+R4dity4@users.noreply.github.com>
2023-07-25 16:03:50 +0700

    Aritmatika.js

---
+++ Aritmatika.js
@@ -0,0 +1,15 @@
+let bil1=8;
+                let bil2=7;
+                //Operasi aritmatika
+             
+                let pertambahan =bil1+bil2;
+                let pengurangan =bil1-bil2;
+                let perkalian =bil1*bil2;
+                let pembagian =bil1/bil2;
+                let modulus =bil1%bil2;
+               
+                document.write ("hasil pertambahan"+pertambahan+"<br>");
+                document.write ("hasil pengurangan"+pengurangan+"<br>");
+                document.write ("hasil perkalian"+perkalian+"<br>");
+                document.write ("hasil pembagian"+pembagian+"<br>");
+                document.write ("hasil modulus"+modulus+"<br>");
